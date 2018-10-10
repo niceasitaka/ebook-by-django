8e66662ce9957581e13af0ea714c500a9a3871dc
niceasitaka <niceasitaka@naver.com>
2018-10-10 12:54:30 +0900

    django-debug-toolbar 추가

--- ebook/config/urls.py
+++ ebook/config/urls.py
@@ -17,3 +17,9 @@ urlpatterns = [
 	path('ebook/', include('ebook.urls', namespace='ebook')),
 	path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+	import debug_toolbar
+	urlpatterns += [
+		path('__debug__/', include(debug_toolbar.urls)),
+	]
